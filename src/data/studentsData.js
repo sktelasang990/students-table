b94ef0092ffeac558c8bdc6049d4f72bdd92255e
sktelasang990 <sktelasang990@gmail.com>
2026-03-12 18:24:34 +0530

    Updated student name

--- src/data/studentsData.js
+++ src/data/studentsData.js
@@ -1,8 +1,7 @@
 const studentsData = [
   { id: 1, name: "Shrishail Telasang", email: "sktelasang990@gmail.com", age: 26 },
   { id: 2, name: "Satish Bilagi", email: "svbilagi123@gmail.com", age: 26 },
-  { id: 3, name: "Veeresh Jamkahndi", email: "vrjamkhandi456@gmail.com", age: 24 },
-  { id: 4, name: "Savitha M", email: "savitham997@gmail.com", age: 32 }
+  { id: 3, name: "Veeresh Jamkahndi", email: "vrjamkhandi456@gmail.com", age: 24 }
 ];
 
 export default studentsData;
